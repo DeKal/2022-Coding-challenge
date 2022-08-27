cd7339be9af996578a8c9875c19825a8df0c1067
nmht <nmhoangthien@gmail.com>
2022-08-27 11:39:47 +0700

    commit UI

---
+++ app/location_tracking/api.js
@@ -0,0 +1,15 @@
+// fetchAPI => return data
+const url = "http://127.0.0.1:5500";
+
+const fetchAPI = async () => {
+	try {
+		console.log("fetch ongoing");
+		const response = await fetch(url);
+		console.log("fetch done: ", response.data);
+		return response.data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+export default fetchAPI;
